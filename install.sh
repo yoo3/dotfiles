658e2bb11ab972edc75a222e6edfb6fd73c8e353
yoo3 <yoosan94@gmail.com>
2023-01-22 23:29:04 +0900

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,24 @@ cd $HOME/dotfiles/
 
 echo $PWD
 
+folders=`find . -type d \
+	! -path "*/.git/*" \
+	! -name .git`
 
+# create symlinks (will overwrite old dotfiles)
+for folder in ${folders}; do
+    #echo "Creating symlink to $file in home directory."
+    mkdir -p $HOME/${folder#./} 2>/dev/null 
+done
+
+files=`find . \
+  -type f \
+  ! -path "./.git/*" \
+  ! -name ./.gitignore \
+  ! -name install.sh`      
 
 # create symlinks (will overwrite old dotfiles)
 for file in ${files}; do
-    echo "Creating symlink to $file in home directory."
-    #ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+    #echo "Creating symlink to $file in home directory."
+    ln -sf $PWD/${file#./} $HOME/${file#./} 
 done
