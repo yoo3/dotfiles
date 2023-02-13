d9c26f2c164fc20368358f402bb59848d5303e3d
yoo3 <yoosan94@gmail.com>
2023-02-13 23:46:39 +0900

    make skhd timeout

--- .bin/macos/skhd_timeout.sh
+++ .bin/macos/skhd_timeout.sh
@@ -2,7 +2,7 @@
 
 
 TIME_STAMP_DIR=$HOME/.bin/tmp/skhd_timeout
-if [ ! -e some_directory ];
+if [ ! -e $TIME_STAMP_DIR ];
 then
   mkdir -p $TIME_STAMP_DIR
 fi
@@ -11,16 +11,48 @@ TIME_STAMP_FILE=$TIME_STAMP_DIR/timestamp
 
 if [[ "$1" =~ ^[0-9]+$ ]];
 then
-  echo 'is number'
+  if [ $$ -ne $(pgrep -fo "$0") ]; then
+    #echo "起動済みです。"
+    exit 1
+  fi
+  #echo 'is number'
   touch $TIME_STAMP_FILE
+
+
+  while true 
+  do
+
+    if [ ! -f $TIME_STAMP_FILE ];
+    then
+      skhd -k "escape"
+      exit 0
+    fi
+
+    NOW=$(date "+%s")
+    TIME_STAMP=$(date -r $TIME_STAMP_FILE "+%s")
+
+    DIFF=$(($NOW - $TIME_STAMP ))
+
+    echo "$TIME_STAMP - $NOW = $DIFF"
+
+    if (( $DIFF >= $1 ));
+    then
+      skhd -k "escape"
+      rm $TIME_STAMP_FILE
+      exit 0
+    fi
+
+    sleep 0.5
+  done
+
 else
   case $1 in
     reset )
-      echo 'reset!!'
+      #echo 'reset!!'
       touch $TIME_STAMP_FILE
       ;;
     kill )
-      echo 'kill!!'
+      #echo 'kill!!'
       rm $TIME_STAMP_FILE
       ;;
   esac
